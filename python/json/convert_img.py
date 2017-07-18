16984511232a6e3a9be810feb610394df7890281
JimmyJiao <jim1949@163.com>
2017-07-18 19:00:07 +0800

    Add map json for convert_img.py

--- python/json/convert_img.py
+++ python/json/convert_img.py
@@ -21,11 +21,13 @@ from PIL import Image
 import sys
 import json
 import yaml
+import time
 
 def listener():
     
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    # rospy.sleep(3)
     sub = rospy.Subscriber("map_status", Int32, convert)
     rospy.spin()
 
@@ -39,18 +41,33 @@ def convert(data):
     im.save(arg+".jpg")
     im.save(arg+".jpeg")
 
+    fr_yaml=open(arg+'.yaml','r')
+    x=yaml.load(fr_yaml)
+
+    rospy.loginfo(x['origin'][1])
+
     pic_size=im.size
     x_size=pic_size[0]
     y_size=pic_size[1]
-    measurements=[{'x_size': x_size,  
-                                    'y_size': y_size }]
-    with open(arg+'.json','w') as fw:
-        fw.write(json.dumps(measurements,sort_keys=True,indent=4, separators=(',', ': ')))
-
-
-
-    
-    
+    measurements={'x_size': x_size,  
+                                    'y_size': y_size }
+    map_json={
+    'createdAt' : time.strftime("%Y-%m-%d %X", time.localtime()),
+    'mapInfo': {
+        'gridHeight': x_size,
+        'gridWidth': y_size,
+        'originX': x['origin'][1],
+        'originY': x['origin'][2],
+        'resolution': x['resolution']},
+    'obstacleFileName' : "unknown.json",
+    'name': arg,
+   
+    'pgmFileName' : arg+'.pgm',
+    'pngFileName' : arg+'.png',
+    'yamlFileName' : arg+'.yaml',
+    }
+    with open(arg+'map.json','w') as fw:
+        fw.write(json.dumps(map_json,sort_keys=True,indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
@@ -59,22 +76,3 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         pass
 
-
-{
-    "createdAt" : "2017-03-25 15:57:55",
-    "id" : "4fec88ea-3c2c-4640-a6fd-ac8a728e9ddd",
-    "mapInfo" : 
-    {
-        "gridHeight" : 480,
-        "gridWidth" : 736,
-        "originX" : -30.400000000000002,
-        "originY" : -11.200000000000001,
-        "resolution" : 0.05000000074505806
-    },
-    "name" : "1",
-    "obstacleFileName" : "7f903e82-acea-444a-975a-cf8addbda877.json",
-    "pgmFileName" : "0eb1a3fa-56cc-45f6-a137-282fa9570650.pgm",
-    "pngFileName" : "3f975d03-a803-4483-b62d-dc2341f13eb4.png",
-    "slopeFileName" : "",
-    "yamlFileName" : "8026bee3-dc73-4ade-a4e3-a6ad102db86a.yaml"
-}
